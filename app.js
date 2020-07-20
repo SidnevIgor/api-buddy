b99e52eb8a6c9bcc2f1641a17e584bbfa3a702eb
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-20 19:08:48 +0300

    Adding core OS, filesystem, path modules

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 let log = require('./logger');
 log('message');
+
+
+//Standard Node packages
+
+//Path module
+const path = require('path');
+const pathObj = path.parse(__filename);
+console.log(pathObj);
+
+
+//OS module
+const os = require('os');
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log(`The total memory of this machine is ${totalMemory} and free memory is ${freeMemory}`);
+
+//FileSystem
+const fs = require('fs');
+fs.readdir('./', (err, files) => {
+  if(err) console.log('Error happened');
+  else {
+     console.log(files);
+  }
+});
