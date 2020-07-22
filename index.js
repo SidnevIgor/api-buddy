453fc5ec812b1cbb25229cf936e82ac52d1a0c96
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-22 14:57:04 +0300

    Adding new course to the array

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
+app.use(express.json()); //enabling JSON parsing
+
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
 app.get('/api/courses', (req, res) => {
   res.send([1,2,3]);
 });
-
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  if(!course) res.status(404).send('There is no course with such id');
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  let course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000; //getting local variables
