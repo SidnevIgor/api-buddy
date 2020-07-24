ae99aeb9539e5ff6be67aab2886fb6c42e19a36e
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-24 18:41:17 +0300

    Adding stores route

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 
 const books = require('./books/books');
 const customers = require('./customers/customers');
+const stores = require('./stores/stores');
 
 app.use(express.json()); //enabling JSON parsing
 app.use('/api/books', books);
 app.use('/api/customers', customers);
+app.use('/api/stores', stores);
 
 const port = process.env.PORT || 3000; //getting local variables
 app.listen(port);
