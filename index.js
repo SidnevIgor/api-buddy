67d3508ecb544db6b08b24cf5fb9a7bf3e2af51d
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-24 13:47:42 +0300

    Adding customer route

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const books = require('./books/books');
+const customers = require('./customers/customers');
 
 app.use(express.json()); //enabling JSON parsing
 app.use('/api/books', books);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000; //getting local variables
 app.listen(port);
