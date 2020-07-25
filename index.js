d1cc4bfe509761a6762da8c982ea5381950e1966
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-25 17:57:38 +0300

    fixing a bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const books = require('./books/books');
 const customers = require('./customers/customers');
 const stores = require('./stores/stores');
-const module = require('./orders/orders');
+const orders = require('./orders/orders');
 
 app.use(express.json()); //enabling JSON parsing
 app.use('/api/books', books);
