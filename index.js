70f26bcc1d649deac488e015f0049b404aa5713b
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-26 13:27:07 +0300

    employees module added

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const books = require('./books/books');
 const customers = require('./customers/customers');
 const stores = require('./stores/stores');
 const orders = require('./orders/orders');
+const employees = require('./employees/employees');
 
 app.use(express.json()); //enabling JSON parsing
 app.use('/api/books', books);
 app.use('/api/customers', customers);
 app.use('/api/stores', stores);
 app.use('/api/orders', orders);
+app.use('/api/employees', employees);
 
 const port = process.env.PORT || 3000; //getting local variables
 app.listen(port);
