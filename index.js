afee0761cc8a6b98267f0e540ab121cf84e8f5a2
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-28 17:29:54 +0300

    Adding loca machine variable

--- index.js
+++ index.js
@@ -20,4 +20,5 @@ app.use('/api/employees', employees);
 const port = process.env.PORT || 3000; //getting local variables
 app.listen(port);
 console.log(`Listening on port ${port}`);
-console.log(`Environment: ${config.get('name')}`)
+console.log(`Environment: ${config.get('name')}`);
+console.log(`App password is ${config.get('password')}`);
