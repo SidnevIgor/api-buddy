4fd90f287c872074087b540c77fdeef474ae3883
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-07-29 14:25:26 +0300

    Adding db connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const config = require('config'); //allows to use local variables
 const helmet = require('helmet'); //protecting routes
+const mongoose = require('mongoose');
 
 const home = require('./home/home');
 const books = require('./books/books');
@@ -23,4 +24,9 @@ const port = process.env.PORT || 3000; //getting local variables
 app.listen(port);
 console.log(`Listening on port ${port}`);
 console.log(`Environment: ${config.get('name')}`);
-console.log(`App password is ${config.get('password')}`);
+mongoose.connect('mongodb://localhost/playground').then(() => {
+  console.log('Successfully connected to the database');
+})
+.catch((err) => {
+  console.log(`Error happened during the conenction to MongoDB: ${err}`);
+})
