c7ab4029a58ae7735fd9bc9f5aeab85b40d935f0
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-03 13:44:47 +0300

    Adding sorting after finding correct items

--- books/books.js
+++ books/books.js
@@ -31,14 +31,13 @@ router.get('/', async (req, res) => {
   let findVal = req.query.title?req.query.title:req.query.author?req.query.author:req.query.genre?req.query.genre:req.query.price?req.query.price:req.query.issueDate?req.query.issueDate:req.query.publisher?req.query.publisher:null;
   let books = [];
   if(selector) {
-    books = await Book.find({ [selector]: [findVal] });
+    books = await Book.find({ [selector]: [findVal] }).sort({ [req.query.sortBy]: 1 });
     res.send(books);
   }
   else {
-    books = await Book.find();
+    books = await Book.find().sort({ [req.query.sortBy]: 1 });
     res.send(books);
   }
-//  .sort({ [req.query.sortBy]: 1 });
 });
 router.get('/:id', validateId, async (req, res) => {
   let book = await Book.findById(req.params.id);
