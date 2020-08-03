053a1e7602437aea397a9653d935a604f08203fc
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-03 12:53:11 +0300

    Adding find condition

--- books/books.js
+++ books/books.js
@@ -27,8 +27,8 @@ const schema = Joi.object({
 
 
 router.get('/', async (req, res) => {
-  let books = await Book.find()
-  .sort({ [req.query.sortBy]: 1 });
+  let books = await Book.find({$or: [{ title: req.query.title }, { title: {$ne: null } }]});
+//  .sort({ [req.query.sortBy]: 1 });
   res.send(books);
 });
 router.get('/:id', validateId, async (req, res) => {
