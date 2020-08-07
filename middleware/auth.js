ef391eae14147a5a852f03a9fca095cc844b46ef
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-07 16:06:37 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,17 @@
-module.exports = function(req,res,next) {
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function auth(req, res, next) {
   let token = req.header('x-auth-token');
   if(!token) return res.status(401).send('This route requires authentication');
   else {
-    next();
+    try {
+      let decoded = jwt.verify(token, config.get('secret')); //we try to decode the token using private key
+      req.user = decoded; //we store a user ID in req now and can access it leter on
+      next();
+    }
+    catch(ex) {
+      res.status(400).send('Invalid token');
+    }
   }
 }
