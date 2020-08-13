1cedc9097405d3aa73d7ecb085bef2eeb62d343c
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-13 18:39:59 +0300

    Update books.test.js

--- tests/books.test.js
+++ tests/books.test.js
@@ -1,18 +1,36 @@
 const request = require('supertest');
 let server;
+const { Book } = require('../books/bookSchema');
 
 describe('/api/books', function() {
   beforeEach(() => {
     server = require('../index');
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Book.remove({});
   });
 
   describe('GET all books', () => {
-    it('should return all genres', async () => {
+    it('should return all books', async () => {
+      await Book.collection.insertMany([
+        {title: 't1'},
+        {title: 't2'},
+        {title: 't3'}
+      ])
       let res = await request(server).get('/api/books');
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+    });
+  });
+  describe('GET one book', () => {
+    it('should retun one book', async () => {
+      let book = new Book({
+        title: 't1'
+      })
+      let bookSaved = await book.save();
+      let res = await request(server).get(`/api/books/${bookSaved._id}`);
+      expect(res.body.title).toMatch(bookSaved.title);
     });
   });
 });
