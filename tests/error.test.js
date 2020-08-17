cdb4786721df5b8b75fb872bd410fe8dffa4217c
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-17 16:26:24 +0300

    validating error middleware

---
+++ tests/error.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+let server;
+
+describe('Testing error validation', () => {
+  beforeEach(() => {
+    if(!server) {
+      server = require('../index');
+    }
+  });
+  afterEach(async () => {
+    server.close();
+  });
+
+  it('should catch an error', async () => {
+    let res = await request(server).get('');
+    expect(res.status).toBe(404);
+  })
+})
