087cca282fad8fa1e16ca1bf6fd1d4f24cc3ab09
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-20 12:50:14 +0300

    Adding auth store validation

---
+++ tests/auth-stores.test.js
@@ -0,0 +1,202 @@
+const request = require('supertest');
+let server, employee, token;
+const { Store } = require('../stores/storeSchema');
+const { Employee } = require('../employees/employerSchema');
+
+describe('/api/auth/stores', function() {
+  beforeEach( async () => {
+    if(!server) {
+      server = require('../index');
+    }
+    employee = new Employee({
+      firstName: "Igor2",
+      lastName: "Sidnev",
+      storeId: "5f355ce806f38631fc33530d",
+      position: "manager"
+    });
+    employee = await employee.save();
+
+    customer = new Customer({
+      firstName: "name5",
+      lastName: "lname4",
+      email: "test@gmail.com",
+      tel: "123-456-789"
+    });
+    let result = await customer.save();
+    token = jwt.sign({_id: result._id}, config.get('secret'));
+  });
+  afterEach(async () => {
+    server.close();
+    await Store.remove({});
+    await Employee.remove({});
+  });
+
+  describe('GET all stores', () => {
+    it('should return 401 error', async () => {
+      await Store.collection.insertMany([
+        {title: 't1'},
+        {title: 't2'},
+        {title: 't3'}
+      ]);
+      let res = await request(server).get('/api/auth/stores');
+      expect(res.status).toBe(401);
+    });
+    it('should return 400 error', async () => {
+      await Store.collection.insertMany([
+        {title: 't1'},
+        {title: 't2'},
+        {title: 't3'}
+      ]);
+      let res = await request(server).get('/api/auth/stores').set('x-auth-token', '1234');
+      expect(res.status).toBe(400);
+    });
+    it('should return all stores', async () => {
+      await Store.collection.insertMany([
+        {title: 't1'},
+        {title: 't2'},
+        {title: 't3'}
+      ]);
+      let res = await request(server).get('/api/auth/stores').set('x-auth-token', token);
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+    });
+    it('should return a Store with an additional parameter', async () => {
+      await Store.collection.insertMany([
+        {title: 't1', price: 100},
+        {title: 't2', price: 200},
+        {title: 't3', price: 300}
+      ]);
+      let res = await request(server).get('/api/auth/stores?price=200').set('x-auth-token', token);
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(1);
+    });
+    it('should return 404 when no Store is found', async () => {
+      await Store.collection.insertMany([
+        {title: 't1', price: 100}
+      ]);
+      let res = await request(server).get('/api/auth/stores?price=200').set('x-auth-token', token);
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe('GET one Store', () => {
+    it('should throw an error when id is invalid', async () => {
+      let res = await request(server).get(`/api/auth/stores/1234`).set('x-auth-token', token);
+      expect(res.status).toBe(404);
+    });
+    it('should throw an error when id is not found', async () => {
+      let res = await request(server).get(`/api/auth/stores/5f355ce806f38631fc33530d`).set('x-auth-token', token);
+      expect(res.status).toBe(404);
+    });
+    it('should retun one Store', async () => {
+      let Store = new Store({
+        title: 't1'
+      })
+      let storesaved = await Store.save();
+      let res = await request(server).get(`/api/auth/stores/${storesaved._id}`).set('x-auth-token', token);
+      expect(res.body.title).toMatch(storesaved.title);
+    });
+  });
+
+  describe('POST one Store', () => {
+    it('should return validation error', async () => {
+      let res = await request(server).post('/api/auth/stores').set('x-auth-token', token).send({
+        title: 't1',
+        price: 100
+      });
+      expect(res.status).toBe(400);
+    });
+    it('should post a Store', async () => {
+      let Store = {
+        title: "Store9",
+        author: "Leo Tolstoy",
+        genre: "Romance",
+        price: 100,
+        issueDate: "2020",
+        publisher: "Alpina"
+      };
+      let res = await request(server).post('/api/auth/stores').set('x-auth-token', token).send({...Store});
+      expect(res.body.title).toMatch(Store.title);
+    });
+  });
+
+  describe('PUT one Store', () => {
+    it('should throw an error when id is invalid', async () => {
+      let Store = {
+        title: "Store9",
+        author: "Leo Tolstoy",
+        genre: "Romance",
+        price: 100,
+        issueDate: "2020",
+        publisher: "Alpina"
+      };
+      let res = await request(server).put(`/api/auth/stores/1234`).set('x-auth-token', token).send(Store);
+      expect(res.status).toBe(404);
+    });
+    it('should throw an error when validation is not passed', async () => {
+      let Store = {
+        title: "Store9"
+      };
+      let res = await request(server).put(`/api/auth/stores/5f2178c4b1ef5441280c2366`).set('x-auth-token', token).send(Store);
+      expect(res.status).toBe(400);
+    });
+    it('should throw an error when id is not found', async () => {
+      let Store = {
+        title: "Store9",
+        author: "Leo Tolstoy",
+        genre: "Romance",
+        price: 100,
+        issueDate: "2020",
+        publisher: "Alpina"
+      };
+      let res = await request(server).put(`/api/auth/stores/5f2178c4b1ef5441280c2366`).set('x-auth-token', token).send(Store);
+      expect(res.status).toBe(400);
+    });
+    it('should put an object in db', async () => {
+      let Store = {
+        title: "Store9",
+        author: "Leo Tolstoy",
+        genre: "Romance",
+        price: 100,
+        issueDate: "2020",
+        publisher: "Alpina"
+      };
+      let savedStore = await Store.collection.insertMany([{...Store}]);
+
+      let res = await request(server).put(`/api/auth/stores/${savedStore.ops[0]._id}`).set('x-auth-token', token).send({
+        title: "Store9",
+        author: "Leo Tolstoy",
+        genre: "Romance",
+        price: 300,
+        issueDate: "2020",
+        publisher: "Alpina"
+      });
+      expect(res.body.price).toEqual(100);
+    });
+  });
+
+  describe('DELETE one Store', () => {
+    it('should throw an error when ID validation is not passed', async () => {
+      let res = await request(server).delete('/api/auth/stores/1234').set('x-auth-token', token);
+      expect(res.status).toBe(404);
+    });
+    it('should throw an error when ID is not found', async () => {
+      let res = await request(server).delete('/api/auth/stores/5f2178c4b1ef5441280c2366').set('x-auth-token', token);
+      expect(res.status).toBe(400);
+    });
+    it('should delete one Store', async () => {
+      let Store = {
+        title: "Store9",
+        author: "Leo Tolstoy",
+        genre: "Romance",
+        price: 100,
+        issueDate: "2020",
+        publisher: "Alpina"
+      };
+      let savedStore = await Store.collection.insertMany([{...Store}]);
+
+      let res = await request(server).delete(`/api/auth/stores/${savedStore.ops[0]._id}`).set('x-auth-token', token);
+      expect(res.body.deletedCount).toBe(1);
+    });
+  })
+});
