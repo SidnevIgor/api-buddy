1978fd70925d57d33a6d2f14051fd47ea0be1124
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-08-31 11:32:57 +0300

    removing id from middleware

--- middleware/cleanResponse.js
+++ middleware/cleanResponse.js
@@ -4,14 +4,13 @@ module.exports = function(obj) {
     for (let i = 0; i < obj.length; i++) {
       let ob = {...obj[i]._doc};
       delete ob._id;
-      delete obj.__v;
       arr.push(ob);
     }
     return arr;
   }
   else {
-    console.log('Entered delete condition');
-    delete obj[i]._id, obj[i].__v;
+    let ob = { ...obj };
+    delete ob._id;
     return obj;
   }
 }
