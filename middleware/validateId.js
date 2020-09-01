78d947617c5dc09632f5d4fb333d9e94a0d650c6
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-01 17:01:03 +0300

    Fixing book id search

--- middleware/validateId.js
+++ middleware/validateId.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = function(req, res, next) {
-  if(!mongoose.Types.ObjectId.isValid(req.params.id)){
-    return res.status(404).send('The id has a wrong structure');
+  if(isNaN(req.params.id)) {
+    return res.status(404).send('The id has to be an integer');
   }
   else{
     next();
