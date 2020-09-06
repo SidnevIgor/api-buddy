31014cad75083f9e55638791cb946f838f0b6e5f
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-06 20:01:07 +0300

    Update stores.test.js

--- tests/stores.test.js
+++ tests/stores.test.js
@@ -54,19 +54,20 @@ describe('/api/stores', function() {
 
   describe('GET one Store', () => {
     it('should throw an error when id is invalid', async () => {
-      let res = await request(server).get(`/api/stores/1234`);
+      let res = await request(server).get(`/api/stores/abc1234`);
       expect(res.status).toBe(404);
     });
     it('should throw an error when id is not found', async () => {
-      let res = await request(server).get(`/api/stores/5f355ce806f38631fc33530d`);
+      let res = await request(server).get(`/api/stores/2`);
       expect(res.status).toBe(404);
     });
     it('should retun one Store', async () => {
       let store = new Store({
+        storeId: 1,
         city: 't1'
       })
       let storeSaved = await store.save();
-      let res = await request(server).get(`/api/stores/${storeSaved._id}`);
+      let res = await request(server).get(`/api/stores/${storeSaved.storeId}`);
       expect(res.body.city).toMatch(storeSaved.city);
     });
   });
@@ -74,6 +75,7 @@ describe('/api/stores', function() {
   describe('POST one Store', () => {
     it('should return validation error', async () => {
       let res = await request(server).post('/api/stores').send({
+        storeId: 1,
         city: 't1',
         building: 100
       });
@@ -81,22 +83,24 @@ describe('/api/stores', function() {
     });
     it('should return a 400 error when employeeId is wrong', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: ['1234']
+        employees: ['abc1234']
       };
       let res = await request(server).post('/api/stores').send({...store});
       expect(res.status).toBe(400);
     });
     it('should post a Store', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       };
       let res = await request(server).post('/api/stores').send({...store});
       expect(res.body.city).toMatch(store.city);
@@ -106,94 +110,101 @@ describe('/api/stores', function() {
   describe('PUT one Store', () => {
     it('should throw an error when id is invalid', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       };
-      let res = await request(server).put(`/api/stores/1234`).send(store);
+      let res = await request(server).put(`/api/stores/abc1234`).send(store);
       expect(res.status).toBe(404);
     });
     it('should throw an error when validation is not passed', async () => {
       let store = {
         city: "Store9"
       };
-      let res = await request(server).put(`/api/stores/5f2178c4b1ef5441280c2366`).send(store);
+      let res = await request(server).put(`/api/stores/1`).send(store);
       expect(res.status).toBe(400);
     });
     it('should throw an error when id is not found', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       };
-      let res = await request(server).put(`/api/stores/5f2178c4b1ef5441280c2366`).send(store);
+      let res = await request(server).put(`/api/stores/2`).send(store);
       expect(res.status).toBe(400);
     });
     it('should put an object in db', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       };
       let savedStore = await Store.collection.insertMany([{...store}]);
 
-      let res = await request(server).put(`/api/stores/${savedStore.ops[0]._id}`).send({
+      let res = await request(server).put(`/api/stores/${savedStore.ops[0].storeId}`).send({
+        storeId: 1,
         city: 'Ufa',
         street: 'Novosibirskaya',
         building: '7',
         postcode: '107497',
-        employees: ['1234']
+        employees: ['abc1234']
       });
       expect(res.status).toBe(400);
     });
     it('should put an object in db', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       };
       let savedStore = await Store.collection.insertMany([{...store}]);
 
-      let res = await request(server).put(`/api/stores/${savedStore.ops[0]._id}`).send({
+      let res = await request(server).put(`/api/stores/${savedStore.ops[0].storeId}`).send({
+        storeId: 1,
         city: 'Ufa',
         street: 'Novosibirskaya',
         building: '7',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       });
-      expect(res.body.building).toEqual("6");
+      expect(res.body.building).toEqual("7");
     });
   });
 
   describe('DELETE one Store', () => {
     it('should throw an error when ID validation is not passed', async () => {
-      let res = await request(server).delete('/api/stores/1234');
+      let res = await request(server).delete('/api/stores/abc1234');
       expect(res.status).toBe(404);
     });
     it('should throw an error when ID is not found', async () => {
-      let res = await request(server).delete('/api/stores/5f2178c4b1ef5441280c2366');
+      let res = await request(server).delete('/api/stores/2');
       expect(res.status).toBe(400);
     });
     it('should delete one Store', async () => {
       let store = {
+        storeId: 1,
         city: 'Moscow',
         street: 'Novosibirskaya',
         building: '6',
         postcode: '107497',
-        employees: [employee._id]
+        employees: [1]
       };
       let savedStore = await Store.collection.insertMany([{...store}]);
 
-      let res = await request(server).delete(`/api/stores/${savedStore.ops[0]._id}`);
-      expect(res.body.deletedCount).toBe(1);
+      let res = await request(server).delete(`/api/stores/${savedStore.ops[0].storeId}`);
+      expect(res.body.storeId).toBe(1);
     });
   })
 });
